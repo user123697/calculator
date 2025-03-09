ace2d8d76159c65d697f8f5010e6a175bd326216
user123697 <santoshtuladhar26@gmail.com>
2025-03-09 08:00:09 +0000

    second commit-C skeleton file

--- calculator.c
+++ calculator.c
@@ -1,1 +1,47 @@
 #Code for making a calculator
+#include <stdio.h>
+
+int main() 
+{
+    double num1, num2, result;
+    char operator;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter first number: ");
+    scanf("%lf", &num1);
+
+    printf("Enter second number: ");
+    scanf("%lf", &num2);
+
+    switch (operator) 
+    {
+        case '+':
+            result = num1 + num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '-':
+            result = num1 - num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '*':
+            result = num1 * num2;
+            printf("Result: %.2lf\n", result);
+            break;
+        case '/':
+            if (num2 != 0) {
+                result = num1 / num2;
+                printf("Result: %.2lf\n", result);
+            } else {
+                printf("Error! Division by zero.\n");
+            }
+            break;
+        default:
+            printf("Invalid operator!\n");
+            break;
+    }
+
+    return 0;
+}
+
